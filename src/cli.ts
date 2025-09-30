1f9934cf355488f09b676f20f4c182ffa94f8d3c
Adriano Raiano <adriano@raiano.ch>
2025-09-30 08:28:18 +0200

    build

--- src/cli.ts
+++ src/cli.ts
@@ -21,7 +21,7 @@ const program = new Command()
 program
   .name('i18next-cli')
   .description('A unified, high-performance i18next CLI.')
-  .version('0.9.17')
+  .version('0.9.18')
 
 program
   .command('extract')
